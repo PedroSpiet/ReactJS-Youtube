15447d8c3686e0b21ebce4eb835fbf7afb75f401
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-18 19:11:45 -0300

    SideBar do Youtube

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { makeStyles, AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'
-import { Menu, AccountCircle, VideoCall, MoreVert, Apps } from '@material-ui/icons'
+import { makeStyles, AppBar, Toolbar, IconButton, Button, Drawer, List, Typography, Divider, ListItem, ListItemText, ListItemIcon, Box} from '@material-ui/core'
+import { Menu, AccountCircle, VideoCall, MoreVert, Apps, Whatshot, Subscriptions, VideoLibrary, HistoryOutlined, AddCircle } from '@material-ui/icons'
+import  HomeIcon  from '@material-ui/icons/Home'
+
 
 const usingStyles = makeStyles((theme) => ({
     root: {
         height: '100vh'
     },
     appBar: {
-        boxShadow: 'none'
+        boxShadow: 'none',
+        zIndex: theme.zIndex.drawer + 1
+    },
+    drawer: {
+        width: 240,
+        flexShrink: 0
+    },
+    drawerPaper: {
+        width: 240
     },
     menuIcon: {
         paddingRight: theme.spacing(5),
@@ -21,6 +31,12 @@ const usingStyles = makeStyles((theme) => ({
     },
     img: {
         width: '8%'
+    },
+    draw: {
+        baground: 'red'
+    },
+    listItemText: {
+        fontSize: 14
     }
 }))
 
@@ -28,6 +44,7 @@ function Home() {
     const classes = usingStyles()
 
     return (
+        <div>
         <AppBar color="inherit" className={classes.appBar}>
         <Toolbar>
           <IconButton className={classes.menuIcon} color="inherit">
@@ -61,6 +78,131 @@ function Home() {
               </Button>
         </Toolbar>
       </AppBar>
+
+      <Drawer 
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+                <ListItem button>
+                    <ListItemIcon>{<HomeIcon />} </ListItemIcon>
+                    <ListItemText classes={{
+                        primary: classes.listItemText
+                    }}
+                    primary={'Inicio'}
+                    >                        
+                    </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>{<Whatshot />} </ListItemIcon>
+                    <ListItemText classes={{
+                        primary: classes.listItemText
+                    }}
+                    primary={'Em Alta'}
+                    >                        
+                    </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>{<Subscriptions />} </ListItemIcon>
+                    <ListItemText classes={{
+                        primary: classes.listItemText
+                    }}
+                    primary={'Inscrições'}
+                    >                        
+                    </ListItemText>
+                </ListItem>
+
+          </List>
+          <Divider />
+          <List>
+          <ListItem button>
+                <ListItemIcon>{<HistoryOutlined />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Histórico'}
+                >                        
+                </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                <ListItemIcon>{<VideoLibrary />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Biblioteca'}
+                >                        
+                </ListItemText>
+                </ListItem>
+          </List>
+
+          <Divider />
+          <Box p={5}>
+              <Typography variant='body1'>
+                  Faça Login para Curtir vídeos, comentar e se inscrever
+              </Typography>
+              <Box mt={2}>
+                  <Button 
+                    size='large'
+                    variant='outlined'
+                    color="secondary"
+                    startIcon={<AccountCircle />}
+                    >
+                        Fazer Login
+                    </Button>
+              </Box>
+          </Box>
+          <Divider />
+          <List>
+          <ListItem button>
+                <ListItemIcon>{<AddCircle />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Filmes'}
+                >                        
+                </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                <ListItemIcon>{<AddCircle />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Jogos'}
+                >                        
+                </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                <ListItemIcon>{<AddCircle />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Música'}
+                >                        
+                </ListItemText>
+                </ListItem>
+
+                <ListItem button>
+                <ListItemIcon>{<AddCircle />} </ListItemIcon>
+                <ListItemText classes={{
+                    primary: classes.listItemText
+                 }}
+                primary={'Esporte'}
+                >                        
+                </ListItemText>
+                </ListItem>
+          </List>
+        </div>
+      </Drawer>
+</div>
     )
 }
 export default Home
